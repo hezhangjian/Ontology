e86de702cf2f7afe7c8ecde8cc987dbc42fb80a7
haodi-jia <396310954@qq.com>
2026-07-24 16:43:02 +0800

    feat(ontology): add postgres jpa persistence
    Signed-off-by: Zhangjian He <hezhangjian97@gmail.com>

---
+++ src/main/java/com/hezhangjian/ontology/repo/OntologyRepository.java
@@ -0,0 +1,6 @@
+package com.hezhangjian.ontology.repo;
+
+import com.hezhangjian.ontology.entity.OntologyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OntologyRepository extends JpaRepository<OntologyEntity, String> {}
